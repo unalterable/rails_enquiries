2705d8d4522f2cfc1abeff545c3ece993b85dd9c
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-21 13:16:37 +0100

    First test passing

--- sextant-altered/lib/rails/routes.rb
+++ sextant-altered/lib/rails/routes.rb
@@ -3,8 +3,8 @@ module ActionDispatch::Routing
     # Includes mount_sextant method for routes. This method is responsible to
     # generate all needed routes for sextant
     def mount_sextant
-      get "rails/routes" => "sextant/routes#index"
-      mount Sextant::Engine => "/sextant", :as => "sextant_engine"
+      get "rails_enquiries/routes" => "sextant/routes#index"
+      # mount Sextant::Engine => "/sextant", :as => "sextant_engine"
     end
   end
 end
